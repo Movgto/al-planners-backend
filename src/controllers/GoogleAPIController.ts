6a665c379539e8836f023327e7fcb4e47d788d9d
Gumaro Monroy <maromvz@gmail.com>
2024-06-17 12:55:37 -0600

    refactor: Update GoogleAPIController to handle event creation

--- src/controllers/GoogleAPIController.ts
+++ src/controllers/GoogleAPIController.ts
@@ -1,6 +1,8 @@
 import type {Request, Response} from 'express'
 import auth2client from '../config/googleapi'
-import { google } from 'googleapis'
+import { google, calendar_v3 } from 'googleapis'
+import colors from 'colors'
+import { handleInternalError } from '../helpers'
 
 class GoogleAPIController {
   static getAuthUrl = (req: Request, res: Response) => {
@@ -13,8 +15,7 @@ class GoogleAPIController {
       res.json({url})
     } catch (error) {
       const errorMessage = 'Something went wrong while trying to generate an auth url'
-      console.log(errorMessage, error)
-      res.status(500).json({error: errorMessage})
+      handleInternalError(error, errorMessage, res)
     }
   }
 
@@ -27,20 +28,43 @@ class GoogleAPIController {
       auth2client.setCredentials(tokens)
 
       const calendarAPI = google.calendar({version: 'v3', auth: auth2client})
-
+      
       const events = await calendarAPI.events.list({
         calendarId: process.env.CALENDAR_ID,
         timeMin: new Date().toISOString(),
         singleEvents: true,
         orderBy: 'startTime'
-      })      
+      })
 
       res.json({events: events.data.items})
     } catch (error) {
+      console.log(colors.bgMagenta('An error happened while trying to connect to Google Calendar API'))
       console.log(error)
       res.status(500).json({
         error: 'Hubo un problema al conectarse con el calendario de Google'
       })
+    }
+  }
+
+  static createEvent = async (req: Request, res: Response) => {
+    const {code} = req.query
+    const event = req.body
+
+    try {
+      const {tokens} = await auth2client.getToken(code as string)
+
+      auth2client.setCredentials(tokens)
+
+      const calendarAPI = google.calendar({version: 'v3', auth: auth2client})
+
+      await calendarAPI.events.insert({
+        auth: auth2client,
+        calendarId: process.env.CLIENT_ID,
+        requestBody: event
+      })
+
+    } catch (error) {      
+      handleInternalError(error, 'An error happened while trying to create an event', res)
     }
   }
 }
