996d0642ee7ebe939c5194da90e0743ef79a90ed
Gumaro Monroy <maromvz@gmail.com>
2024-06-17 12:55:52 -0600

    feat: Add Event model for storing calendar events

---
+++ src/models/Event.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IEvent extends Document {
+    summary: string,
+    start: {
+        dateTime: string
+    }
+    end: {
+        dateTime: string        
+    },
+    sentToCalendar: boolean
+}
+
+const rangeSchema : Schema = new Schema({
+    dateTime: String
+})
+
+const eventSchema : Schema = new Schema({
+    summary: {
+        type: String,
+        required: true
+    },
+    start: {        
+        type: rangeSchema,
+        required: true
+    },
+    end: {
+        type: rangeSchema,
+        required: true
+    },
+    sentToCalendar: {
+        type: Boolean,
+        default: false
+    }
+}, {timestamps: true})
+
+const Event = mongoose.model<IEvent>('Event', eventSchema)
+
+export default Event
