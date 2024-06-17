ae6be9e5a0c59a2f6ec18e92f6f52df8e80d8311
Gumaro Monroy <maromvz@gmail.com>
2024-06-17 12:55:22 -0600

    feat: Add EventsController with getEvents and createEvent methods

---
+++ src/controllers/EventsController.ts
@@ -0,0 +1,27 @@
+import Event from "../models/Event"
+import {Request, Response} from 'express'
+import { handleInternalError } from "../helpers"
+class EventsController {
+    static getEvents = async (req: Request, res: Response) => {
+        try {
+            const events = await Event.find()
+
+            res.json({events})
+        } catch (error) {
+            handleInternalError(error, 'Algo fallo al obtener los eventos', res)
+        }
+    }
+
+    static createEvent = async (req: Request, res: Response) => {
+        const event = req.body
+
+        try {
+            await Event.create(event)
+            res.send('Nuevo evento creado exitosamente!\nRecuerda sincronizar tus eventos.')
+        } catch (error) {
+            handleInternalError(error, 'Algo fallo al intentar crear un evento', res)
+        }
+    }
+}
+
+export default EventsController
