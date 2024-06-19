e00c72009f57ca404ed8d2fbb2388925d97ec084
Gumaro Monroy <maromvz@gmail.com>
2024-06-18 21:45:17 -0600

    feat: Add input validation for creating events

--- src/routes/eventsRouter.ts
+++ src/routes/eventsRouter.ts
@@ -1,10 +1,32 @@
 import {Router} from 'express'
 import EventsController from '../controllers/EventsController'
+import { body } from 'express-validator'
+import inputValidation from '../middleware/inputValidation'
 
 const router = Router()
 
 router.get('/',
     EventsController.getEvents
+)
+
+router.post('/',
+    body('summary').notEmpty().withMessage('Summary must not be empty'),
+    body('start').custom(val => {
+        if (typeof val === 'object' && val.dateTime) {
+            return true
+        }
+
+        throw new Error('Invalid start object')
+    }),
+    body('end').custom(val => {
+        if (typeof val === 'object' && val.dateTime) {
+            return true
+        }
+
+        throw new Error('Invalid start object')
+    }),
+    inputValidation,
+    EventsController.createEvent
 )
 
 export default router
