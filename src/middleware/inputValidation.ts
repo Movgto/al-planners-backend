ef5c6c7af9954ff9c5781b56ca3b39e8df4caa92
Gumaro Monroy <maromvz@gmail.com>
2024-06-18 21:45:04 -0600

    refactor: Fix input validation error handling in inputValidation middleware

--- src/middleware/inputValidation.ts
+++ src/middleware/inputValidation.ts
@@ -2,10 +2,12 @@ import {validationResult} from 'express-validator'
 import type {Request, Response, NextFunction} from 'express'
 
 const inputValidation = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.body)
   const result = validationResult(req)
+  console.log(result)
 
-  if (!result.isEmpty) {
-    return res.json({error: result.array})
+  if (!result.isEmpty()) {
+    return res.status(400).json({error: result.array()})
   }
 
   next()
