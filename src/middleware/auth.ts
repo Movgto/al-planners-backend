555661ad968a24c0c72518c0830a39de989271c1
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 21:03:27 -0600

    feat: Add authentication middleware for admin routes

---
+++ src/middleware/auth.ts
@@ -0,0 +1,48 @@
+import {Request, Response, NextFunction} from 'express'
+import User, { IUser } from '../models/User'
+import { verify } from 'jsonwebtoken'
+import { handleInternalError } from '../helpers'
+
+declare global {
+  namespace Express {
+    interface Request {
+      user: IUser
+    }
+  }
+}
+
+export const auhtenticateAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  const bearer = req.headers.authorization
+
+  try {
+    if (!bearer) {
+      return res.status(401).json({error: 'Acción no autorizada'})
+    }
+
+    const token = bearer.split(' ')[1]
+
+    const userData = verify(token, process.env.JWT_SECRET!)
+
+    if (typeof userData === 'object' && userData.id) {
+
+      const userExists = await User.findById(userData.id).select('id email name admin')
+
+      if (!userExists) {
+        return res.status(404).json({error: 'Usuario no encontrado'})
+      }
+
+      if (!userExists.admin) {
+        return res.status(401).json({error: 'Acción no autorizada'})
+      }
+
+      req.user = userExists
+
+    } else {
+      res.status(401).json({error: 'Acción no autorizada'})
+    }
+  } catch (error) {
+    handleInternalError(error, 'Algo falló al intentar autenticarte', res)
+  }
+
+  next()
+}
