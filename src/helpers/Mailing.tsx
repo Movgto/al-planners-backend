ff7e72d76c46c90e36ad78b5f8645fde8a977b27
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 21:03:22 -0600

    feat: Add Mailing component for appointment notifications

---
+++ src/helpers/Mailing.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Html, Container, Body, Tailwind, Heading, Text, Img } from '@react-email/components'
+
+type MailingProps = {
+  name: string
+  date: string
+  hour: string
+}
+
+export const HtmlForEventNotification = (props: Pick<MailingProps, 'name' | 'hour' | 'date'>) => {
+  return (
+    <Html>
+      <Tailwind>
+        <Body className="bg-pink-300 flex flex-col items-center px-5 py-10">
+          <Container
+            className="flex flex-col gap-4 bg-white rounded-lg p-4"
+          >
+            <Heading
+              className="text-black font-semibold text-xl text-center"
+            >
+              Gracias por agendar tu{' '}
+              <span className='font-bold text-pink-400'>cita</span>{' '}
+              con nosotros!{' '}
+              <span className='font-bold text-pink-400'>{props.name}</span>
+            </Heading>
+            <Img
+              src="cid:alplannerslogo"
+              alt="al planners logo"              
+              className="w-full"              
+            />
+            <Text
+              className='text-center text-lg'
+            >
+              Te estaremos esperando el{' '}
+              <span className='font-bold text-rose-400'>{props.date}</span> a las <strong>{props.hour}</strong>!
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  )
+}
