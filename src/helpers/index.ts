e1674abb4b894a1e3fc31b976cf8167b9f971d75
Gumaro Monroy <maromvz@gmail.com>
2024-06-29 22:10:01 -0600

    fix: Handle timezone conversion correctly in AvailabilityController

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -9,33 +9,6 @@ export const handleInternalError = (error: unknown, errorMsg: string, res: Respo
     res.status(500).json({ error: err.message })
 }
 
-export const isAvailabilityValid = (availability: IAvailabilityTime, availabilityToCompare: IAvailabilityTime) => {
-    const newAvailabilityStartHour = new Date(availability.startTime).getHours()
-    const newAvailabilityEndHour = new Date(availability.endTime).getHours()
-    const availabilityStartHour = new Date(availabilityToCompare.startTime).getHours()
-    const availabilityEndHour = new Date(availabilityToCompare.endTime).getHours()
-
-    const differenceBetweenHours = Math.abs(newAvailabilityStartHour - newAvailabilityEndHour)
-
-    console.log('====== Difference betweenhours ======')
-    console.log(differenceBetweenHours)
-
-    if (differenceBetweenHours < 1) return false
-
-    if (newAvailabilityStartHour === availabilityStartHour) return false
-
-    if (
-        (newAvailabilityStartHour < availabilityStartHour && newAvailabilityEndHour > availabilityStartHour)
-        || (newAvailabilityStartHour < availabilityEndHour && newAvailabilityEndHour > availabilityEndHour)
-        || (availabilityStartHour < newAvailabilityStartHour && availabilityEndHour > newAvailabilityStartHour)
-        || (availabilityStartHour < newAvailabilityEndHour && availabilityEndHour > newAvailabilityEndHour)
-    ) {
-        return false
-    }
-
-    return true
-}
-
 export const dateFormater = (isoDate: string) => {
     const date = new Date(isoDate)
     const displayDate = new Intl.DateTimeFormat('es-MX', {
@@ -60,4 +33,31 @@ export const getDateInTimezoneFromISO = (isoDate: string) => {
 
 export const getDateInTimezone = (date: Date) => {
     return new Date(date.toLocaleString('en-US', {timeZone: 'America/Mexico_City'}))
+}
+
+export const isAvailabilityValid = (availability: IAvailabilityTime, availabilityToCompare: IAvailabilityTime) => {
+    const newAvailabilityStartHour = getDateInTimezone(new Date(availability.startTime)).getHours()
+    const newAvailabilityEndHour = getDateInTimezone(new Date(availability.endTime)).getHours()
+    const availabilityStartHour = getDateInTimezone(new Date(availabilityToCompare.startTime)).getHours()
+    const availabilityEndHour = getDateInTimezone(new Date(availabilityToCompare.endTime)).getHours()
+
+    const differenceBetweenHours = Math.abs(newAvailabilityStartHour - newAvailabilityEndHour)
+
+    console.log('====== Difference betweenhours ======')
+    console.log(differenceBetweenHours)
+
+    if (differenceBetweenHours < 1) return false
+
+    if (newAvailabilityStartHour === availabilityStartHour) return false
+
+    if (
+        (newAvailabilityStartHour < availabilityStartHour && newAvailabilityEndHour > availabilityStartHour)
+        || (newAvailabilityStartHour < availabilityEndHour && newAvailabilityEndHour > availabilityEndHour)
+        || (availabilityStartHour < newAvailabilityStartHour && availabilityEndHour > newAvailabilityStartHour)
+        || (availabilityStartHour < newAvailabilityEndHour && availabilityEndHour > newAvailabilityEndHour)
+    ) {
+        return false
+    }
+
+    return true
 }
