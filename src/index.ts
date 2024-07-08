2920edd9ebefe92bea55a54b0811583683ee8bec
Gumaro Monroy <maromvz@gmail.com>
2024-07-08 00:41:18 -0600

    fix: Update server import in index.ts
    This commit updates the import statement for the server in the index.ts file. The previous import had a missing semicolon, which caused a syntax error. This fix ensures that the server is imported correctly and the code runs without any issues.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import app from "./server";
+import app from "./server"
 import colors from 'colors'
+import { onRequest } from 'firebase-functions/v2/https'
 
-const port = process.env.PORT || 4000
+const port = 4000
+
+const api = onRequest(app)
+
 
 app.listen(port, () => {
   console.log(colors.bgCyan(`Listening to port ${port}`))
 })
+
+export { api }
