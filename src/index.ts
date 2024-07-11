0ba6cd1c57a5abff46de6bb3dcb8bb9c2e64945d
Gumaro Monroy <maromvz@gmail.com>
2024-07-11 16:37:46 -0600

    ```text refactor: Comment out firebase-functions import in index.ts
    The firebase-functions import in the index.ts file has been commented out. This change is necessary to temporarily disable the use of firebase-functions during development. The import will be uncommented before deployment.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import app from "./server"
 import colors from 'colors'
-import { onRequest } from 'firebase-functions/v2/https'
+// import { onRequest } from 'firebase-functions/v2/https'
 
-const port = 4000
+const port = process.env.PORT || 4000
 
-const api = onRequest(app)
+// const api = onRequest(app)
 
 
 app.listen(port, () => {
   console.log(colors.bgCyan(`Listening to port ${port}`))
 })
 
-export { api }
+// export { api }
