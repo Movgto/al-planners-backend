e5b7a186b91f7d3d2835c387f9c6f48fc98c47ba
Gumaro Monroy <maromvz@gmail.com>
2024-08-31 12:48:05 -0600

    refactor: Add Admin model for managing admin accounts
    A new Admin model has been added to the src/models/Admin.ts file. This model defines the schema for admin accounts, including the name, email, and password fields. This change allows for separate management of admin and user accounts, improving security and organization within the application.

---
+++ src/models/Admin.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, Schema } from "mongoose"
+
+export interface IAdmin extends Document {
+  name: string
+  email: string
+  password: string
+}
+
+const AdminSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+const Admin = mongoose.model<IAdmin>('Admin', AdminSchema)
+
+export default Admin
